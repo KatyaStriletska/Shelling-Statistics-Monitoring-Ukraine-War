70bbe59f1cc2444b4ad12135a070b2822a610cd9
kachynskai <105295714+kachynskai@users.noreply.github.com>
2024-10-21 01:02:05 +0300

    create plot for most common models of weapons

--- analysis-data-backend/main.py
+++ analysis-data-backend/main.py
@@ -1,16 +1,23 @@
 from data_processing import load_and_process_data
-# from visualization import plot_launch_places_distribution, plot_time_series
+
+
+# from visualization import plot_total_launched_and_destroyed_per_year_itaractive
+from visualization import chart_most_common_weapons_per_year
 
 def main():
     # Шлях до вашого CSV файлу
-    file_path = "data/missile_attacks_daily.csv"
+    file_path = "/Users/admin/Documents/Shelling-Statistics-Monitoring-Ukraine-War/data/missile_attacks_daily.csv"
 
     # Завантажуємо та обробляємо дані
     df_massive_attacks = load_and_process_data(file_path)
-    
+
     # Створюємо графіки
     # plot_launch_places_distribution(df_massive_attacks, )
     # plot_time_series(df_massive_attacks)
+    chart_most_common_weapons_per_year(df_massive_attacks, 2023)
+
+    # plot_total_launched_and_destroyed_per_year_itaractive(df_massive_attacks, 2024)
+
 
 if __name__ == "__main__":
     main()
--- analysis-data-backend/visualization.py
+++ analysis-data-backend/visualization.py
@@ -1,20 +1,39 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
+from collections import Counter
+import plotly.graph_objects as go
 
 
-def plot_total_launched_and_destroyed_per_year(df: pd.DataFrame, year: int):
-    df_year = df[df['time_start'].dt.year == year]
-    df_year['month'] = df_year['time_start'].dt.month
-    monthly_stats = df_year.groupby('month').size().reset_index(name='count')
-    plt.figure(figsize=(8, 6)) # Width and Height of the chart
-sns.lineplot(x='x',
-             y='y',
-             data=df,
-             marker='o', # Style used to mark the join between 2 points
-            )
-plt.xlabel('X-axis') # x-axis name
-plt.ylabel('Y-axis') # y-axis name
-plt.title('Simple Connected Scatter Plot') # Add a title
-plt.show() # Display the graph
-    # print(monthly_stats)
+def chart_most_common_weapons_per_year(data: pd.DataFrame, year: int):
+    data_by_years = data[data["time_start"].dt.year == year]
+    # print(data_by_years.groupby(["model"]).count())
+
+    counter = Counter(data_by_years["model"])
+    top_10 = counter.most_common(10)
+    total_count = len(data_by_years["model"])
+
+    models = []
+    percentages = []
+
+    for value, count in top_10:
+        models.append(value)
+        percentages.append((count / total_count) * 100)
+
+    models.append("Other")
+    percentages.append(100 - sum(percentages))
+
+    print(models)
+    print(percentages)
+
+    colors = ["#2b472f", "#3e5334", "#4a6047", "#556a48", "#6d8162", "#718970",
+              "#889a80", "#9fb29e", "#a6b899", "#c0cfc0", "#d8e8d1"]
+
+    fig = go.Figure(data=[go.Pie(labels=models, values=percentages,
+                                 hoverinfo="none",
+                                 textinfo="none",
+                                 marker=dict(colors=colors),
+                                 hovertemplate="Model: %{label}<br>Percentage: %{percent}<extra></extra>",
+                                 hole=0.5)])
+
+    fig.update_layout(title_text="Most common models of weapons")
+
+    fig.show()
