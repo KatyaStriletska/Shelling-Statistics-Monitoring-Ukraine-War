7167fbcc7de11bfff68fed06680ba6d143a6750d
kachynskai <105295714+kachynskai@users.noreply.github.com>
2024-10-20 23:31:13 +0300

    reformatting files

--- analysis-data-backend/analysisData.py
+++ analysis-data-backend/analysisData.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 
-df_massive_attacks = pd.read_csv("data\missile_attacks_daily.csv")
+df_massive_attacks = pd.read_csv("/Users/admin/Documents/Shelling-Statistics-Monitoring-Ukraine-War/data/missile_attacks_daily.csv")
+
 
 # preprocessing data for missile_attacks_daily.csv
 
@@ -9,11 +10,12 @@ df_massive_attacks = pd.read_csv("data\missile_attacks_daily.csv")
 # print(f"Data types before: \n {df_massive_attacks.dtypes} \nShape: {df_massive_attacks.shape}")
 
 def parse_datetime(val):
-    val = val.strip() 
+    val = val.strip()
     try:
         return pd.to_datetime(val, format='%Y-%m-%d %H:%M', errors='raise')
     except ValueError:
         return pd.to_datetime(val, format='%Y-%m-%d') + pd.Timedelta(hours=0)
+
 
 df_massive_attacks["time_start"] = df_massive_attacks["time_start"].apply(parse_datetime)
 df_massive_attacks["time_end"] = df_massive_attacks["time_end"].apply(parse_datetime)
@@ -22,7 +24,8 @@ df_massive_attacks["time_end"] = df_massive_attacks["time_end"].apply(parse_date
 
 # Checking for null/missing values
 # print("\nSum of null/missing values: \n", df_massive_attacks.isnull().sum())
-df_massive_attacks = df_massive_attacks.drop(["launched_details", "launch_place_details", "still_attacking", "cross_border_belarus"],  axis=1)
+df_massive_attacks = df_massive_attacks.drop(
+    ["launched_details", "launch_place_details", "still_attacking", "cross_border_belarus"], axis=1)
 # print("\nSum of null/missing values after dropping: \n", df_massive_attacks.isnull().sum())
 
 # Checking for duplicate values
@@ -32,8 +35,7 @@ df_massive_attacks = df_massive_attacks.drop(["launched_details", "launch_place_
 # print(df_massive_attacks.describe())
 
 
-
-# Transform empty 'launch_place' values 
+# Transform empty 'launch_place' values
 
 df_massive_attacks['launch_place'] = df_massive_attacks['launch_place'].str.split(' and ')
 df_massive_attacks = df_massive_attacks.explode('launch_place')
@@ -45,11 +47,9 @@ most_common_places = df_massive_attacks['launch_place'].value_counts()
 # print(most_common_places)
 
 
-
 # Transform 'back_russia' column with replacing Nan value
 # if the value is zero, it means that nothing was returned back to russia
 df_massive_attacks["back_russia"] = df_massive_attacks["back_russia"].fillna(0.0)
-
 
 # Transform 'not_reach_goal' column with replacing Nan value
 # if the value is zero, it means that nothing was reach 
