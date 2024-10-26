8f250ee7eb13f4b05622d0451f462d29085f909e
Artem <artem.levchenko04@gmail.com>
2024-10-27 00:16:14 +0300

    modify shelling map

--- analysis-data-backend/map_visualization.py
+++ analysis-data-backend/map_visualization.py
@@ -1,23 +1,55 @@
 import pandas as pd
 import folium
-from folium.plugins import HeatMap
+from folium.plugins import HeatMap, TimestampedGeoJson
+import json
+from datetime import datetime
 
 def shelling_map_visualization(data: pd.DataFrame) -> folium.Map:
     ukraine_map = folium.Map(location=[48.3794, 31.1656], zoom_start=6)
 
-    # Add a marker for each shelling incident
+    features = []
     for _, row in data.iterrows():
-        folium.Marker(
-            location=[row['latitude'], row['longitude']],
-            popup=f"Date: {row['source_date']}<br>",
-            icon=folium.Icon(color='red', icon='fire')
-        ).add_to(ukraine_map)
+        feature = {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [row['longitude'], row['latitude']]
+            },
+            'properties': {
+                'time': row['date_start'].strftime('%Y-%m-%d'),
+                'popup': f"Date: {row['date_start']}<br>Deaths of civilians: {row['deaths_civilians']}<br>{row['source_headline']}",
+                "icon": "circle",
+                "iconstyle": {
+                    "fillColor": "red",
+                    "fillOpacity": 0.6,
+                    "stroke": "false",
+                    "radius": 15,
+                },
+                "style": {"weight": 0}
+            }
+        }
+        features.append(feature)
 
-    # Optional: Add heatmap layer for shelling intensity visualization
-    heat_data = [[row['latitude'], row['longitude']] for _, row in data.iterrows()]
-    HeatMap(heat_data, radius=8, max_zoom=13).add_to(ukraine_map)
+    # Create TimestampedGeoJson
+    timestamped_geojson = TimestampedGeoJson(
+        {
+            'type': 'FeatureCollection',
+            'features': features,
+        },
+        period='P1D',
+        duration='P1D',
+        add_last_point=False,
+        auto_play=False,
+        loop=False,
+        max_speed=1,
+        loop_button=True,
+        date_options='YYYY-MM-DD',
+        time_slider_drag_update=True
+    )
+
+    timestamped_geojson.add_to(ukraine_map)
 
     # Save the map as an HTML file
-    # ukraine_map.save("ukraine_shelling_map.html")
-    
+    #ukraine_map.save("ukraine_shelling_map.html")
+
     return ukraine_map
